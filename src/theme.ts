bfa467053c4c7c74f104081f00cf77d9b734a322
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-10-23 10:23:30 +0100

    installed mui and set up theme

---
+++ src/theme.ts
@@ -0,0 +1,50 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#FEFCF7",
+    },
+    primary: {
+      main: "#0E8784",
+      dark: "#333D4B",
+      light: "#FDD6BA",
+    },
+    secondary: {
+      main: "#FEFCF7",
+      light: "#83888F",
+    },
+  },
+  typography: {
+    fontFamily: ["Barlow", "sans-serif"].join(","),
+    h1: {
+      fontFamily: ["Fraunces", "serif"].join(","),
+      fontSize: 72,
+    },
+
+    h2: {
+      fontFamily: ["Fraunces", "serif"].join(","),
+      fontSize: 40,
+    },
+
+    h3: {
+      fontFamily: ["Fraunces", "serif"].join(","),
+      fontSize: 32,
+    },
+    h4: {
+      fontFamily: ["Fraunces", "serif"].join(","),
+      fontSize: 24,
+    },
+    // TITLE ALTERNATE BIG
+    h5: {
+      fontFamily: ["Fraunces", "serif"].join(","),
+      fontSize: 150,
+    },
+    body1: {
+      fontFamily: ["Barlow", "sans-serif"].join(","),
+      fontSize: 16,
+    },
+  },
+});
+
+export { theme };
