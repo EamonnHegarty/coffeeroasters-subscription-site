39ff85eaace84f17aa55deda4a1c54038d13255f
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-11-20 15:36:07 +0000

    added our commitment section to the about us page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,16 +1,112 @@
+import { Box, Grid, Typography } from "@mui/material";
 import Hero from "../components/Hero";
 import { aboutHeroConfig } from "../config/heroConfig";
+import Commitment from "../assets/about/desktop/image-commitment.jpg";
 
 const AboutUs = () => {
   return (
-    <Hero
-      page={aboutHeroConfig.page}
-      title={aboutHeroConfig.title}
-      description={aboutHeroConfig.description}
-      desktopBackground={aboutHeroConfig.desktopBackground}
-      tabletBackground={aboutHeroConfig.tabletBackground}
-      mobileBackground={aboutHeroConfig.mobileBackground}
-    />
+    <>
+      <Hero
+        page={aboutHeroConfig.page}
+        title={aboutHeroConfig.title}
+        description={aboutHeroConfig.description}
+        desktopBackground={aboutHeroConfig.desktopBackground}
+        tabletBackground={aboutHeroConfig.tabletBackground}
+        mobileBackground={aboutHeroConfig.mobileBackground}
+      />
+      <Box>
+        <Grid container alignItems="center" justifyContent="center" p={5}>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            lg={6}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              component="img"
+              src={Commitment}
+              sx={{
+                width: "100%",
+                height: "auto",
+                maxHeight: {
+                  lg: 500,
+                  xl: 600,
+                },
+                maxWidth: {
+                  xs: 400,
+                  lg: 500,
+                },
+                borderRadius: "10px",
+                mb: {
+                  xs: 2,
+                  md: 0,
+                },
+              }}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={6}
+            lg={6}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              px: {
+                xs: 0,
+                md: 7,
+              },
+            }}
+          >
+            <Typography
+              variant="h3"
+              sx={{
+                textAlign: {
+                  xs: "center",
+                  lg: "left",
+                },
+                color: "primary.dark",
+                mb: 3,
+              }}
+            >
+              Our Commitment
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                color: "primary.dark",
+                textAlign: {
+                  xs: "center",
+                  lg: "left",
+                },
+              }}
+            >
+              We’re built on a simple mission and a commitment to doing good
+              along the way. We want to make it easy for you to discover and
+              brew the world’s best coffee at home. It all starts at the source.
+              To locate the specific lots we want to purchase, we travel nearly
+              60 days a year trying to understand the challenges and
+              opportunities in each of these places. We collaborate with
+              exceptional coffee growers and empower a global community of
+              farmers through with well above fair-trade benchmarks. We also
+              offer training, support farm community initiatives, and invest in
+              coffee plant science. Curating only the finest blends, we roast
+              each lot to highlight tasting profiles distinctive to their native
+              growing region.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
 };
 
